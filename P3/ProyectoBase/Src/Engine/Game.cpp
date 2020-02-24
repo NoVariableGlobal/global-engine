90b6aeb6719b9bbb1aa5626e07db3af5073885db
Miriam <mirileis@ucm.es>
2020-02-24 19:32:40 +0100

    Game.cpp y Game.h

---
+++ P3/ProyectoBase/Src/Engine/Game.cpp
@@ -0,0 +1,53 @@
+#include "Game.h"
+#include "ApplicationContext.h"
+#include "Scene.h"
+
+Game::Game()
+{
+	//...
+	initContext();
+	//...
+	initScenes();
+	//...
+}
+
+Game::~Game()
+{
+	//...
+	currentScene = nullptr;
+	for (int i = 0; i < scenesQueue.size(); i++)
+	{
+		delete scenesQueue[i];
+		scenesQueue[i] = nullptr;
+	}
+	//...
+}
+
+void Game::initContext()
+{
+	//...
+	ApplicationContext();
+	//...
+}
+
+void Game::initScenes()
+{
+	//...
+}
+
+void Game::update()
+{
+	//...
+	while (!exit)
+	{
+		//...
+		//currentScene.update();
+		//...
+	}
+	//...
+}
+
+void Game::setScene(std::string scene)
+{
+	//...
+}
