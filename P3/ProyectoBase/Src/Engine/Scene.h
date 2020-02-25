a1babf0837f726ee214f050e9f10869cdc582cc3
CarlosMGM <gonzalem@ucm.es>
2020-02-25 10:50:38 +0100

    updated Scene.h

--- P3/ProyectoBase/Src/Engine/Scene.h
+++ P3/ProyectoBase/Src/Engine/Scene.h
@@ -1,24 +1,29 @@
 #pragma once
 #include <map>
 #include <vector>
-
+#include <string>
 class string;
 class Component;
 class Entity;
 
 
-typedef string Id;
+typedef std::string Id;
 
 class Scene {
 
   std::map<Id*, Entity*> entities;
-  std::vector<Component*> componentList;
 
 public:
   Scene();
   ~Scene();
 
 
+  // Given the name of the scene, reads its respective file and tells the Engine to create all entities and component
   void load(string name);
-  void update();
+
+
+  //void update();
+
+  // Search the entity in map and returns a reference to it.
+  Entity* getEntitybyId(Id id);
 };
