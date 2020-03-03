6344eaf813f2fd145e1d5a2fabed8dc8a8c72ab5
Miriam <mirileis@ucm.es>
2020-03-03 21:23:06 +0100

    SDL añadido al proyecto

--- P3/ProyectoBase/Src/Engine/Application.cpp
+++ P3/ProyectoBase/Src/Engine/Application.cpp
@@ -1,7 +1,8 @@
 #include "ApplicationContext.h"
+#include <SDL.h>
 
 #ifdef _DEBUG
-int main()
+int main(int argc, char* args[])
 #else
 #include <windows.h>
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, LPSTR lmCmdLine, int nCmdShow)
