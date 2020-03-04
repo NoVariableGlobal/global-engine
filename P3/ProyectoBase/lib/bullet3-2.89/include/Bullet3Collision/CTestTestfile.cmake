224a5df7023a44f43859d8c4209cb55d848b9f2b
GonzaPM7 <gonzap03@ucm.es>
2020-03-04 12:15:01 +0100

    add: physics dependencies

---
+++ P3/ProyectoBase/lib/bullet3-2.89/include/Bullet3Collision/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: U:/hlocal/bullet3-2.89/src/Bullet3Collision
+# Build directory: U:/hlocal/global-engine/P3/ProyectoBase/Depedencias/bullet3-2.89/src/Bullet3Collision
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
