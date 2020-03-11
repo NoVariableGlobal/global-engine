a4a7588fbf5e1487edfb7708c821083e2571a4c1
GonzaPM7 <gonzap03@ucm.es>
2020-03-11 13:00:39 +0100

    add:createRb

--- P3/ProyectoBase/Src/Engine/RigidbodyPC.cpp
+++ P3/ProyectoBase/Src/Engine/RigidbodyPC.cpp
@@ -1,7 +1,9 @@
 #include "RigidbodyPC.h"
+#include "PhysicsContext.h"
 
-RigidbodyPC::RigidbodyPC()
+RigidbodyPC::RigidbodyPC(Ogre::Vector3 _pos, Ogre::Vector3 _shape, float _mass, PhysicsContext* _physics)
 {
+	body = _physics->createRB(_pos, _shape, _mass);
 }
 
 RigidbodyPC::~RigidbodyPC()
--- P3/ProyectoBase/Src/Engine/RigidbodyPC.h
+++ P3/ProyectoBase/Src/Engine/RigidbodyPC.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "PhysicsComponent.h"
+#include "PhysicsContext.h"
 
 class RigidbodyPC : public PhysicsComponent {
+private:
+	btRigidBody* body;
+	bool trigger;
 public:
-	RigidbodyPC();
+	RigidbodyPC(Ogre::Vector3 _pos, Ogre::Vector3 _shape, float _mass, PhysicsContext* _physics);
 	~RigidbodyPC();
 };
