71bc25f984096b74b82bae17c00d324a88ad5260
GonzaPM7 <gonzap03@ucm.es>
2020-03-12 12:57:54 +0100

    add: addForce && setGravity

--- P3/ProyectoBase/Src/Engine/RigidbodyPC.cpp
+++ P3/ProyectoBase/Src/Engine/RigidbodyPC.cpp
@@ -1,11 +1,36 @@
 #include "RigidbodyPC.h"
+#include "PhysicsComponent.h"
 #include "PhysicsContext.h"
 
-RigidbodyPC::RigidbodyPC(Ogre::Vector3 _pos, Ogre::Vector3 _shape, float _mass, PhysicsContext* _physics)
+
+RigidbodyPC::RigidbodyPC(Ogre::Vector3 _pos, Ogre::Vector3 _shape, float _mass, PhysicsContext* _physics, bool _trigger)
 {
 	body = _physics->createRB(_pos, _shape, _mass);
+	trigger = _trigger;
 }
 
 RigidbodyPC::~RigidbodyPC()
 {
+}
+
+void RigidbodyPC::update()
+{
+}
+
+void RigidbodyPC::addForce(const Ogre::Vector3& _force, Ogre::Vector3 _relative_pos)
+{
+	if (_relative_pos == Ogre::Vector3(0.0f, 0.0f, 0.0f))
+		body->applyCentralForce((btVector3(btScalar(_force.x), btScalar(_force.y), btScalar(_force.z))));
+	else
+		body->applyForce((btVector3(btScalar(_force.x), btScalar(_force.y), btScalar(_force.z))), (btVector3(btScalar(_relative_pos.x), btScalar(_relative_pos.y), btScalar(_relative_pos.z))));
+}
+
+void RigidbodyPC::setGravity(const Ogre::Vector3& _g)
+{
+	body->setGravity(btVector3(btScalar(_g.x), btScalar(_g.y), btScalar(_g.z)));
+}
+
+bool RigidbodyPC::isTrigger() const
+{
+	return trigger;
 }
--- P3/ProyectoBase/Src/Engine/RigidbodyPC.h
+++ P3/ProyectoBase/Src/Engine/RigidbodyPC.h
@@ -3,11 +3,22 @@
 #include "PhysicsComponent.h"
 #include "PhysicsContext.h"
 
+#include <btBulletDynamicsCommon.h>
+#include <btBulletCollisionCommon.h>
+#include "LinearMath/btIDebugDraw.h"
+#include "DebugDrawer.h"
+#include "OgreSDLContext.h"
+
 class RigidbodyPC : public PhysicsComponent {
 private:
-	btRigidBody* body;
+	btRigidBody* body = nullptr;
 	bool trigger;
 public:
-	RigidbodyPC(Ogre::Vector3 _pos, Ogre::Vector3 _shape, float _mass, PhysicsContext* _physics);
+	RigidbodyPC(Ogre::Vector3 _pos, Ogre::Vector3 _shape, float _mass, PhysicsContext* _physics, bool _trigger);
 	~RigidbodyPC();
+	virtual void update();
+
+	void addForce(const Ogre::Vector3& _force, Ogre::Vector3 _relative_pos = { 0.0f, 0.0f, 0.0f });
+	bool isTrigger() const;
+	void setGravity(const Ogre::Vector3& _g);
 };
