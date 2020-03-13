76cb4572828b3b3255356776e3dae6614d6a4288
Dankgard <dangar18@ucm.es>
2020-03-13 17:49:20 +0100

    WASD console messages
    Experience a realistically inspired recreation of iceland's southern coast.

--- P3/ProyectoBase/Src/Engine/TopDownPlayerMovementIC.cpp
+++ P3/ProyectoBase/Src/Engine/TopDownPlayerMovementIC.cpp
@@ -1,11 +1,13 @@
 #include "TopDownPlayerMovementIC.h"
 #include <SDL.h>
+#include "OgreRoot.h"
 #include "Factory.h"
 #include "FactoriesFactory.h"
 #include "Component.h"
 #include "ComponentsManager.h"
 #include <json.h>
 // #include "RigidbodyPC"
+#include <iostream>
 
 TopDownPlayerMovementIC::TopDownPlayerMovementIC()
 {
@@ -18,20 +20,25 @@ TopDownPlayerMovementIC::~TopDownPlayerMovementIC()
 void TopDownPlayerMovementIC::handleInput(const SDL_Event& _event)
 {
 	if (_event.type == SDL_KEYDOWN) {
+		// TO DO: send messages to rigidbody to move in a specific direction
 		// RigidbodyPC* rb = dynamic_cast<RigidbodyPC*>(_father->getComponent("RigidbodyPC"));
 		switch (_event.key.keysym.sym)
 		{
 		case SDLK_w:
-			// rb->addForce(Ogre::Vector3(0.0f, 0.0f, -1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f));
+			std::cout << '\n' << "MOVE UP" << '\n';
+			// rb->addForce(Ogre::Vector3(0.0f, 0.0f, -_speed), Ogre::Vector3(0.0f, 0.0f, 0.0f));
 			break;
 		case SDLK_s:
-			// rb->addForce(Ogre::Vector3(0.0f, 0.0f, 1.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f);
+			std::cout << '\n' << "MOVE DOWN" << '\n';
+			// rb->addForce(Ogre::Vector3(0.0f, 0.0f, _speed), Ogre::Vector3(0.0f, 0.0f, 0.0f);
 			break;
 		case SDLK_a:
-			// rb->addForce(Ogre::Vector3(-1.0f, 0.0f, 0.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f);
+			std::cout << '\n' << "MOVE LEFT" << '\n';
+			// rb->addForce(Ogre::Vector3(-_speed, 0.0f, 0.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f);
 			break;
 		case SDLK_d:
-			// rb->addForce(Ogre::Vector3(1.0f, 0.0f, 0.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f);
+			std::cout << '\n' << "MOVE RIGHT" << '\n';
+			// rb->addForce(Ogre::Vector3(_speed, 0.0f, 0.0f), Ogre::Vector3(0.0f, 0.0f, 0.0f);
 			break;
 		}
 	}
@@ -42,7 +49,7 @@ void TopDownPlayerMovementIC::setMovementSpeed(float speed)
 	_speed = speed;
 }
 
-// FACTORY INFRASTRUCTURE
+// TO DO: factory isnt called 
 class TopDownPlayerMovementICFactory : public ComponentFactory {
 public:
 	TopDownPlayerMovementICFactory() {};
--- P3/ProyectoBase/Src/Engine/TopDownPlayerMovementIC.h
+++ P3/ProyectoBase/Src/Engine/TopDownPlayerMovementIC.h
@@ -1,5 +1,12 @@
 #pragma once
 #include "InputComponent.h"
+
+namespace Ogre {
+	typedef float Real;
+	template <int dims, typename T> class Vector;
+	typedef Vector<3, Real> Vector3;
+}
+
 class TopDownPlayerMovementIC : public InputComponent
 {
 private:
