fc6fd8d2b13956e64b450b8541bca49dbd8e2bef
Carlos Gonzalez <gonzalem@ucm.es>
2020-03-16 00:52:47 +0100

    finished spawnerEnemies

--- P3/ProyectoBase/Src/Engine/SpawnerEnemies.cpp
+++ P3/ProyectoBase/Src/Engine/SpawnerEnemies.cpp
@@ -1,5 +1,19 @@
 #include "SpawnerEnemies.h"
+#include "Scene.h"
+#include "Entity.h"
+#include "TransformComponent.h"
 
 void SpawnerEnemies::checkEvent()
 {
+
+  Entity* newEntity;
+  if (timeToSpawn()) {
+    newEntity = spawnPrefab();
+    if(transform == nullptr)
+      transform = static_cast<TransformComponent*> (father->getComponent("TransformComponent"));
+
+    TransformComponent * spawnTransform = static_cast<TransformComponent*> (newEntity->getComponent("TransformComponent"));
+    scene->addEntity(newEntity);
+    spawnTransform->setPosition(transform->getPosition());
+  }
 }
--- P3/ProyectoBase/Src/Engine/SpawnerEnemies.h
+++ P3/ProyectoBase/Src/Engine/SpawnerEnemies.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Spawner.h"
+class TransformComponent;
 
 class SpawnerEnemies :
   public Spawner
 {
-
+private: 
+  TransformComponent* transform = nullptr;
 public:
   virtual void checkEvent() override;
 
