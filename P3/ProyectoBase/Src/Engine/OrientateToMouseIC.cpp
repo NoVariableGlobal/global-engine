50536b87adf2af3bc228bd875e93585b7ce32fae
Antonio Román <kyradiscord@gmail.com>
2020-03-22 00:04:19 +0100

    style: Formatted code

--- P3/ProyectoBase/Src/Engine/OrientateToMouseIC.cpp
+++ P3/ProyectoBase/Src/Engine/OrientateToMouseIC.cpp
@@ -1,11 +1,11 @@
 #include "OrientateToMouseIC.h"
-#include "Factory.h"
-#include "FactoriesFactory.h"
 #include "ComponentsManager.h"
+#include "Entity.h"
+#include "FactoriesFactory.h"
+#include "Factory.h"
+#include "OgreSDLContext.h"
 #include "Scene.h"
 #include "TransformComponent.h"
-#include "Entity.h"
-#include "OgreSDLContext.h"
 
 #include "OgreRoot.h"
 
@@ -14,53 +14,59 @@
 
 #include <iostream>
 
-void OrientateToMouseIC::handleInput(const SDL_Event& _event)
-{
-	// Get father's transform
-	TransformComponent* fatherTransform = dynamic_cast<TransformComponent*>(father->getComponent("TransformComponent"));
-	Ogre::Vector3 fatherPos = fatherTransform->getPosition();
-	
-	// Get mouse position
-	int xMouse, yMouse;
-	SDL_GetGlobalMouseState(&xMouse, &yMouse);
+void OrientateToMouseIC::handleInput(const SDL_Event& _event) {
+    // Get father's transform
+    TransformComponent* fatherTransform = dynamic_cast<TransformComponent*>(
+        father->getComponent("TransformComponent"));
+    Ogre::Vector3 fatherPos = fatherTransform->getPosition();
 
-	// Get window position
-	SDL_Window* win = OgreSDLContext::getInstance()->getSDLWindow();
-	int xWindow, yWindow;
-	SDL_GetWindowPosition(win, &xWindow, &yWindow);
-	xMouse -= xWindow;
-	yMouse -= yWindow;
+    // Get mouse position
+    int xMouse, yMouse;
+    SDL_GetGlobalMouseState(&xMouse, &yMouse);
 
-	// Get window size
-	int xWinSize, yWinSize;
-	SDL_GetWindowSize(win, &xWinSize, &yWinSize);
+    // Get window position
+    SDL_Window* win = OgreSDLContext::getInstance()->getSDLWindow();
+    int xWindow, yWindow;
+    SDL_GetWindowPosition(win, &xWindow, &yWindow);
+    xMouse -= xWindow;
+    yMouse -= yWindow;
 
-	// Adjust limits
-	if (xMouse < 0) xMouse = 0;
-	if (yMouse < 0) yMouse = 0;
-	if (xMouse > xWinSize) xMouse = xWinSize;
-	if (yMouse > yWinSize) yMouse = yWinSize;
+    // Get window size
+    int xWinSize, yWinSize;
+    SDL_GetWindowSize(win, &xWinSize, &yWinSize);
 
-	Ogre::Vector3 newOri = Ogre::Vector3(abs(fatherPos.x - xMouse), abs(fatherPos.y - yMouse), fatherPos.z);
-	fatherTransform->setOrientation(newOri);
-	//std::cout << fatherTransform->getOrientation().x << " " << fatherTransform->getOrientation().y << "\n";
+    // Adjust limits
+    if (xMouse < 0)
+        xMouse = 0;
+    if (yMouse < 0)
+        yMouse = 0;
+    if (xMouse > xWinSize)
+        xMouse = xWinSize;
+    if (yMouse > yWinSize)
+        yMouse = yWinSize;
+
+    Ogre::Vector3 newOri = Ogre::Vector3(
+        abs(fatherPos.x - xMouse), abs(fatherPos.y - yMouse), fatherPos.z);
+    fatherTransform->setOrientation(newOri);
+    // std::cout << fatherTransform->getOrientation().x << " " <<
+    // fatherTransform->getOrientation().y << "\n";
 }
 
 // FACTORY INFRASTRUCTURE
 class OrientateToMouseICFactory : public ComponentFactory {
-public:
-	OrientateToMouseICFactory() = default;
-	virtual Component* create(Entity* _father, Json::Value& _data, Scene* scene)
-	{
-		// Create the component
-		OrientateToMouseIC* orientationIC = new OrientateToMouseIC();
-		scene->getComponentsManager()->addIC(orientationIC);
+  public:
+    OrientateToMouseICFactory() = default;
+    virtual Component* create(Entity* _father, Json::Value& _data,
+                              Scene* scene) {
+        // Create the component
+        OrientateToMouseIC* orientationIC = new OrientateToMouseIC();
+        scene->getComponentsManager()->addIC(orientationIC);
 
-		// Set the component's father as the entity it was instanced for
-		orientationIC->setFather(_father);
+        // Set the component's father as the entity it was instanced for
+        orientationIC->setFather(_father);
 
-		return orientationIC;
-	};
+        return orientationIC;
+    };
 };
 
 REGISTER_FACTORY("OrientateToMouseIC", OrientateToMouseIC);
