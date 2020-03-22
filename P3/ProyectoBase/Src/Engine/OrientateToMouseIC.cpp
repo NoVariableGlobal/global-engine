d027374b8830cb3e1ff7c34b80e626fc92b3d4d9
Omar <onaranjo@ucm.es>
2020-03-22 11:44:14 +0000

    All working

--- P3/ProyectoBase/Src/Engine/OrientateToMouseIC.cpp
+++ P3/ProyectoBase/Src/Engine/OrientateToMouseIC.cpp
@@ -6,20 +6,18 @@
 #include "OgreSDLContext.h"
 #include "Scene.h"
 #include "TransformComponent.h"
+#include "TridimensionalObjectRC.h"
 
 #include "OgreRoot.h"
 
 #include <SDL.h>
 #include <cstdlib>
+#include <math.h>
+#include <stdlib.h>
 
 #include <iostream>
 
 void OrientateToMouseIC::handleInput(const SDL_Event& _event) {
-    // Get father's transform
-    TransformComponent* fatherTransform = dynamic_cast<TransformComponent*>(
-        father->getComponent("TransformComponent"));
-    Ogre::Vector3 fatherPos = fatherTransform->getPosition();
-
     // Get mouse position
     int xMouse, yMouse;
     SDL_GetGlobalMouseState(&xMouse, &yMouse);
@@ -45,11 +43,22 @@ void OrientateToMouseIC::handleInput(const SDL_Event& _event) {
     if (yMouse > yWinSize)
         yMouse = yWinSize;
 
-    Ogre::Vector3 newOri = Ogre::Vector3(
-        abs(fatherPos.x - xMouse), abs(fatherPos.y - yMouse), fatherPos.z);
-    fatherTransform->setOrientation(newOri);
-    // std::cout << fatherTransform->getOrientation().x << " " <<
-    // fatherTransform->getOrientation().y << "\n";
+    // Get father's position
+    TransformComponent* fatherTransform = dynamic_cast<TransformComponent*>(
+        father->getComponent("TransformComponent"));
+    Ogre::Vector3 fatherPos = fatherTransform->getPosition();
+    fatherPos.x += xWinSize / 2;
+    fatherPos.y += yWinSize / 2;
+
+    // Calculate angle between mouse and father
+    float angleInRad = atan2(fatherPos.y - yMouse, fatherPos.x - xMouse);
+    float angleInDeg = -angleInRad * 180 / M_PI;
+
+    // Make the rotation
+    TridimensionalObjectRC* fatherRender =
+        dynamic_cast<TridimensionalObjectRC*>(
+            father->getComponent("TridimensionalObjectRC"));
+    fatherRender->rotate(angleInDeg - 90, Ogre::Vector3(0, 1, 0));
 }
 
 // FACTORY INFRASTRUCTURE
