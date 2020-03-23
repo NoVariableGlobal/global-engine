af0dbb43d480301aa3b997a9c4067976ef483389
Antonio Román <kyradiscord@gmail.com>
2020-03-24 00:17:04 +0100

    build: Add all other projects, CMake build and configure, and file copy

--- build.ps1
+++ build.ps1
@@ -4,6 +4,7 @@ param (
     [string]
     $MsBuild = "",
 
+    [string] $CMake = "cmake",
     [switch] $Build,
     [switch] $Release,
     [string] $Target,
@@ -53,9 +54,106 @@ If ($MsBuildParameters.Length -Eq 0) {
 }
 
 $RootFolder = "$($PSScriptRoot)"
+$BinaryDirectory = "$RootFolder\P3\ProyectoBase\bin"
+$DependenciesRoot = "$RootFolder\P3\ProyectoBase\lib"
 
-function Step-VisualStudio ([string] $ProjectPath) {
-    & $MsBuild $ProjectPath $MsBuildParameters
+function Step-VisualStudioRaw([string] $Path, [string[]] $Arguments) {
+    & $MsBuild $Path $Arguments
 }
 
+function Step-VisualStudioThirdPartyDebug([string] $Path) {
+    Write-Host "# Now building '" -ForegroundColor Blue -NoNewline
+    Write-Host $Path              -ForegroundColor Cyan -NoNewline
+    Write-Host "' as Debug."      -ForegroundColor Blue
+
+    Step-VisualStudioRaw $Path @("-t:Build", "-p:Configuration=Debug;Platform=x64;WarningLevel=0", "-m", "-maxCpuCount", "-noLogo", "-verbosity:quiet")
+}
+
+function Step-VisualStudioThirdPartyRelease([string] $Path) {
+    Write-Host "# Now building '" -ForegroundColor Blue -NoNewline
+    Write-Host $Path              -ForegroundColor Cyan -NoNewline
+    Write-Host "' as Release."    -ForegroundColor Blue
+
+    Step-VisualStudioRaw $Path @("-t:Build", "-p:Configuration=Release;Platform=x64;WarningLevel=0", "-m", "-maxCpuCount", "-noLogo", "-verbosity:quiet")
+}
+
+function Step-VisualStudio([string] $Path) {
+    Write-Host "# Now building '"             -ForegroundColor Blue -NoNewline
+    Write-Host $Path                          -ForegroundColor Cyan -NoNewline
+    Write-Host "' as $PropertyConfiguration." -ForegroundColor Blue
+
+    Step-VisualStudioRaw $Path $MsBuildParameters
+}
+
+function Step-CopyToBinaryDirectory([string] $From, [string[]] $Paths) {
+    Write-Host "# Now copying " -ForegroundColor Blue -NoNewline
+    Write-Host $Paths.Length    -ForegroundColor Cyan -NoNewline
+    Write-Host " file(s) from " -ForegroundColor Blue -NoNewline
+    Write-Host $From            -ForegroundColor Cyan -NoNewline
+    Write-Host " to '"          -ForegroundColor Blue -NoNewline
+    Write-Host $BinaryDirectory -ForegroundColor Cyan -NoNewline
+    Write-Host "'... "          -ForegroundColor Blue -NoNewLine
+
+    Copy-Item -Path $Paths -Destination $BinaryDirectory
+
+    Write-Host "Finished!"      -ForegroundColor Green
+}
+
+function Step-CMake([string] $Path, [string[]] $Arguments) {
+    New-Item -ItemType Directory -Force -Path "$Path\build"
+    & $CMake -S $Path -B "$Path\build" $Arguments
+}
+
+# Bullet
+$BulletFolder = "$DependenciesRoot\bullet3-2.89"
+Step-CMake $BulletFolder  @(
+    "-DBUILD_BULLET2_DEMOS:BOOL=OFF",
+    "-DBUILD_BULLET3:BOOL=ON",
+    "-DBUILD_CLSOCKET:BOOL=OFF",
+    "-DBUILD_CPU_DEMOS:BOOL=OFF",
+    "-DBUILD_ENET:BOOL=OFF",
+    "-DBUILD_EXTRAS:BOOL=OFF",
+    "-DBUILD_OPENGL3_DEMOS:BOOL=OFF",
+    "-DBUILD_PYBULLET:BOOL=OFF",
+    "-DBUILD_SHARED_LIBS:BOOL=OFF",
+    "-DBUILD_UNIT_TESTS:BOOL=OFF",
+    "-DUSE_MSCV_RUNTIME_LIBRARY_DLL:BOOL=ON"
+)
+Step-VisualStudioThirdPartyDebug "$BulletFolder\build\ALL_BUILD.vcxproj"
+Step-VisualStudioThirdPartyRelease "$BulletFolder\build\ALL_BUILD.vcxproj"
+
+# FMod
+# $FModFolder = "$DependenciesRoot\fmod"
+
+# JsonCPP
+$JsonFolder = "$DependenciesRoot\jsoncpp-master"
+Step-CMake $JsonFolder @()
+Step-VisualStudioThirdPartyRelease "$JsonFolder\build\ALL_BUILD.vcxproj"
+
+# Ogre
+$OgreFolder = "$DependenciesRoot\ogre-1.12.5"
+Step-CMake $OgreFolder @("-DOGRE_BUILD_COMPONENT_OVERLAY:BOOL=OFF")
+Step-VisualStudioThirdPartyDebug "$OgreFolder\build\ALL_BUILD.vcxproj"
+Step-VisualStudioThirdPartyRelease "$OgreFolder\build\ALL_BUILD.vcxproj"
+Step-CopyToBinaryDirectory "Ogre" @(
+    "$OgreFolder\build\bin\debug\OgreMain_d.dll",
+    "$OgreFolder\build\bin\debug\RenderSystem_Direct3D11_d.dll",
+    "$OgreFolder\build\bin\debug\RenderSystem_GL_d.dll",
+    "$OgreFolder\build\bin\debug\OgreRTShaderSystem_d.dll",
+    "$OgreFolder\build\bin\debug\Codec_STBI_d.dll",
+    "$OgreFolder\build\bin\release\OgreMain.dll",
+    "$OgreFolder\build\bin\release\RenderSystem_Direct3D11.dll",
+    "$OgreFolder\build\bin\release\RenderSystem_GL.dll",
+    "$OgreFolder\build\bin\release\zlib.dll",
+    "$OgreFolder\build\bin\release\OgreRTShaderSystem.dll",
+    "$OgreFolder\build\bin\release\Codec_STBI.dll"
+)
+
+# SDL2
+$Sdl2Folder = "$DependenciesRoot\SDL2-2.0.10"
+Step-CopyToBinaryDirectory "SDL2" @(
+    "$Sdl2Folder\lib\x64\SDL2.dll"
+)
+
+# Global Engine
 Step-VisualStudio "$RootFolder\P3\ProyectoBase\ProyectoBase.sln"
