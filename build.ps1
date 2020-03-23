f2cb6cf65723d38a030cbacdcc047a8e2b03d1a1
Antonio Román <kyradiscord@gmail.com>
2020-03-23 22:58:28 +0100

    refactor: Tidy everything, abstract MSVC call in own function

--- build.ps1
+++ build.ps1
@@ -1,60 +1,61 @@
-function Step-Run {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, Position = 0)]
-        [ValidateScript( { Test-Path -LiteralPath $_ -PathType Leaf })]
-        [string]
-        $MsBuild,
+param (
+    [Parameter(Position = 0)]
+    [ValidateScript( { Test-Path -LiteralPath $_ -PathType Leaf })]
+    [string]
+    $MsBuild = "",
 
-        [ValidateScript( { Test-Path -LiteralPath $_ -PathType Leaf })]
-        [string]
-        $ProjectPath = "$($PSScriptRoot)\P3\ProyectoBase\ProyectoBase.sln",
+    [switch] $Build,
+    [switch] $Release,
+    [string] $Target,
+    [string] $Property,
+    [string] $Platform = "x64",
 
-        [switch] $Build,
-        [switch] $Release = $false,
-        [string] $Target,
-        [string] $Property,
-        [string] $Platform = "x64",
+    [Alias("Parallel")]
+    [switch] $SequentialBuild,
+    [switch] $AllCores,
 
-        [Alias("Parallel")]
-        [switch] $BuildInParallel,
-        [switch] $AllCores,
+    [ValidateSet('q', 'quiet', 'm', 'minimal', 'n', 'normal', 'd', 'detailed', 'diag', 'diagnostic')]
+    [string] $Verbosity = "normal",
+    [switch] $DisplayLogo,
 
-        [ValidateSet('q', 'quiet', 'm', 'minimal', 'n', 'normal', 'd', 'detailed', 'diag', 'diagnostic')]
-        [string] $Verbosity = "normal",
-        [switch] $NoLogo,
+    [Alias("Parameters", "Params", "P")]
+    [string[]] $MsBuildParameters
+)
 
-        [Alias("Parameters", "Params", "P")]
-        [string] $MsBuildParameters
-    )
-
-    begin {
-        If ($MsBuildParameters -Eq "") {
-            If ($Target -Eq "" -And (!$Build.IsPresent -Or $Build.ToBool())) {
-                $Target = "-t:Build"
-            }
-
-            If ($Property -Eq "") {
-                $PropertyConfiguration = If ($Release.ToBool()) { "Release" } Else { "Debug" }
-                $Property = "-p:Configuration=$PropertyConfiguration;Platform=$Platform"
-            }
-
-            $BuildInParallelArgument = If (!$BuildInParallel.IsPresent -Or $BuildInParallel.ToBool()) { "-m" } Else { "" }
-            $MaxCpuCountArgument = If (!$AllCores.IsPresent -Or $AllCores.ToBool()) { "-maxCpuCount" } Else { "" }
-            $NoLogoArgument = If (!$NoLogo.IsPresent -Or $NoLogo.ToBool()) { "-noLogo" } Else { "" }
-            $VerbosityArgument = "-verbosity:$Verbosity"
-            $MsBuildParameters = "$Target $Property $VerbosityArgument $BuildInParallelArgument $MaxCpuCountArgument $NoLogoArgument"
-        }
-    }
-
-    process {
-        & $MsBuild $ProjectPath @($Target, $Property, $BuildInParallelArgument, $VerbosityArgument, $MaxCpuCountArgument, $NoLogoArgument)
-    }
-
-    end {
+if ($MsBuild.Length -Eq 0) {
+    $PossibleMsBuild = Resolve-Path "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\*\MSBuild\*\Bin\MSBuild.exe"
+    If ($PossibleMsBuild.Length -Ge 0) {
+        $MsBuild = $PossibleMsBuild[0]
+        Write-Host "MsBuild not providen, using '" -ForegroundColor Blue -NoNewline
+        Write-Host $MsBuild                        -ForegroundColor Cyan -NoNewline
+        Write-Host "' instead."                    -ForegroundColor Blue
         Write-Host ""
-        Write-Host "All jobs finished!"
+    } Else {
+        throw "I could not find MsBuild"
     }
 }
 
-Step-Run
+If ($MsBuildParameters.Length -Eq 0) {
+    If ($Target -Eq "" -And (!$Build.IsPresent -Or $Build.ToBool())) {
+        $Target = "-t:Build"
+    }
+
+    If ($Property -Eq "") {
+        $PropertyConfiguration = If ($Release.ToBool()) { "Release" } Else { "Debug" }
+        $Property = "-p:Configuration=$PropertyConfiguration;Platform=$Platform"
+    }
+
+    $BuildInParallelArgument = If ($SequentialBuild.ToBool()) { "" } Else { "-m" }
+    $MaxCpuCountArgument = If (!$AllCores.IsPresent -Or $AllCores.ToBool()) { "-maxCpuCount" } Else { "" }
+    $NoLogoArgument = If ($DisplayLogo.ToBool()) { "" } Else { "-noLogo" }
+    $VerbosityArgument = "-verbosity:$Verbosity"
+    $MsBuildParameters = @($Target, $Property, $VerbosityArgument, $BuildInParallelArgument, $MaxCpuCountArgument, $NoLogoArgument)
+}
+
+$RootFolder = "$($PSScriptRoot)"
+
+function Step-VisualStudio ([string] $ProjectPath) {
+    & $MsBuild $ProjectPath $MsBuildParameters
+}
+
+Step-VisualStudio "$RootFolder\P3\ProyectoBase\ProyectoBase.sln"
