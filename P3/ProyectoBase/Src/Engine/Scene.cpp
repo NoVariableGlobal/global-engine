32499c90031005f469c1ef4f094d9d92f0324f1a
pablojor <pablojor@ucm.es>
2020-03-25 13:40:33 +0100

    tags

--- P3/ProyectoBase/Src/Engine/Scene.cpp
+++ P3/ProyectoBase/Src/Engine/Scene.cpp
@@ -56,6 +56,16 @@ Entity* Scene::getEntitybyId(std::string id) {
     return assert_find(entities, id);
 }
 
+std::vector<Entity*>& Scene::getEntitiesbyTag(std::string tag) {
+    std::vector<Entity*> tagEntities;
+
+       for (auto it : entities) {
+       if( it.second->getId() == tag);
+            tagEntities.push_back(it.second);
+    }
+       return tagEntities;
+}
+
 void Scene::addEntity(Entity* entity) {
     entities.emplace(entity->getId(), entity);
 }
