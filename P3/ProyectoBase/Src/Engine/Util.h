6211d6340585b51cc45e7f6585491924e2c89b6f
onaranjoUCM <37249723+onaranjoUCM@users.noreply.github.com>
2020-03-28 17:06:19 +0000

    Component removal controled (#56)
    Co-authored-by: pablojor <pablojor@ucm.es>

--- P3/ProyectoBase/Src/Engine/Util.h
+++ P3/ProyectoBase/Src/Engine/Util.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
 template <typename T>
 T assert_find(std::map<std::string, T> map, std::string name) {
@@ -14,4 +15,36 @@ T assert_find(std::map<std::string, T> map, std::string name) {
         throw std::exception("Resource could not be found");
     }
     return it->second;
+}
+
+template <typename T>
+void assert_deleteComponent(std::vector<T>& deleteVector,
+                            std::vector<T>& vector) {
+    if (deleteVector.size() > 0) {
+        bool erased = false;
+        auto itDelete = deleteVector.begin();
+        while (itDelete != deleteVector.end()) {
+            auto it = vector.begin();
+            while (it != vector.end() && !erased) {
+                if ((*it) == (*itDelete)) {
+                    delete *it;
+                    erased = true;
+                } else
+                    ++it;
+            }
+            erased = false;
+            if (it != vector.end())
+                vector.erase(it);
+            else {
+                std::string type = typeid(T).name();
+                std::cout << "ERROR: Component of type '" + type +
+                                 "' could not be deleted\n";
+                throw std::exception("Component could not be deleted, it "
+                                     "probably was already deleted before\n");
+            }
+
+            ++itDelete;
+        }
+        deleteVector.clear();
+    }
 }
