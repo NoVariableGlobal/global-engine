fc09b8d2481aebed59aed7d54c64c8d9ced794f0
pablojor <pablojor@ucm.es>
2020-05-07 11:54:00 +0200

    fullScreen from the start

--- src/OgreSDLContext.cpp
+++ src/OgreSDLContext.cpp
@@ -107,7 +107,7 @@ void OgreSDLContext::createWindow(const std::string& appName) {
             /*EXCEPCION*/
         }
 
-    int flags = 0;
+    int flags = SDL_WINDOW_FULLSCREEN_DESKTOP;
 
     mWindow_.native = SDL_CreateWindow(appName.c_str(), SDL_WINDOWPOS_CENTERED,
                                        SDL_WINDOWPOS_CENTERED, w, h, flags);
